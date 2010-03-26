6b05c359d9201340119f0e8b3c3bbdb63a9abec9
Piotr Usewicz <piotr@layer22.com>
2010-03-26 10:52:54 +0000

    Added ability to create many widgets
    Now backend creates a token (SHA1) per configured widget, which allows us to have many copies of the same widget, but with different configuration.

--- public/sonia.js
+++ public/sonia.js
@@ -1,6 +1,5 @@
-var Sonia = Class.create();
-
-Sonia.prototype = {
+var Sonia = Class.create({
+  widgets: {},
   initialize: function(host) {
     this.host = host;
     this.websocket = new WebSocket(this.host);
@@ -11,78 +10,83 @@ Sonia.prototype = {
     this.websocket.onclose   = this.onclose.bind(this);
     this.websocket.onerror   = this.onerror.bind(this);
   },
-  
-  onopen    : function() {
-      console.log("Socket opened... ");
+  onopen: function() {
+    console.log("Socket opened... ");
   },
-  onmessage : function(event) {
-      var data = event.data;
-      console.log("Received message...", data);
-      this.dispatcher.dispatch(data);
-      // this.websocket.close();
+  onmessage: function(event) {
+    var data = event.data;
+    console.log("Received message...", data);
+    this.dispatcher.dispatch(data);
   },
-  onclose   : function() {
-      console.log("Closing socket... ");
+  onclose: function() {
+    console.log("Closing socket... ");
   },
-  onerror   : function(event) {
-      console.log("Received error:", event);
+  onerror: function(event) {
+    console.log("Received error:", event);
+  },
+  addWidget: function(widget_id, widget) {
+    this.widgets[widget_id] = widget;
   }
-}
+});
 
 var Dispatcher = Class.create({
-    initialize: function(sonia) {
-        this.sonia = sonia;
-        this.twitter = false;
-    },
-    
-    dispatch: function(data) {
-        var payload = JSON.parse(data);
-        
-        if(payload.message) {
-            var message = payload.message;
-            console.log("Dispatching", message.payload, "to", message.widget);
-            this.twitter.receive(message.payload);
-        } else if(payload.setup) {
-            var widgets = payload.setup;
-            // TODO: Setup widgets
-            widgets.each(function(payload) {
-                var widget = payload.widget;
-                var config = payload.config;
-                
-                // TODO: Create real logic
-                this.twitter = new Twitter(config);
-            }.bind(this));
+  initialize: function(sonia) {
+    this.sonia = sonia;
+  },
+  dispatch: function(data) {
+    var json = JSON.parse(data);
+
+    if(json.message) {
+      if(json.message.widget && json.message.widget_id && json.message.payload) {
+        this.sonia.widgets[json.message.widget_id].receive(json.message.payload);
+      } else {
+        console.log("Missing data in message message:", json.message);
+      }
+    } else if(json.setup) {
+      var widgets = json.setup;
+      widgets.each(function(payload) {
+        var widget    = payload.widget;
+        var widget_id = payload.widget_id;
+        var config    = payload.config;
+        if(widget && widget_id && config) {
+          console.log("new " + widget + "(widget_id, config)");
+          var widget_object = eval("new " + widget + "(widget_id, config)");
+          this.sonia.addWidget(widget_id, widget_object);
+        } else {
+          console.log("Missing data in setup message:", json.setup);
         }
+      }.bind(this));
     }
+  }
 });
 
 var Twitter = Class.create({
-    initialize: function(config) {
-        console.log("Building Twitter with", config.title);
-        this.title = config.title;
-        this.max_items = config.nitems;
-        this.messages = [];
-        this.element = "article[widget=twitter]";
-    },
-    
-    receive: function(message) {
-        if(this.messages.length >= this.max_items) {
-            this.messages.shift();
-        }
-        
-        this.messages.push(message);
-        this.update();
-    },
-    
-    update: function() {
-        // remove existing messages
-        $$(this.element + ' p').invoke('remove');
-
-        this.messages.each(function(message) {
-            var cont = new Element('p');
-            cont.appendChild(new Element('a', { href: 'http://www.twitter.com/' + message.user, class: 'author' }).update(message.user));
-            cont.appendChild(document.createTextNode(message.text));
-            $$(this.element)[0].appendChild(cont);
-        }.bind(this));
+  initialize: function(widget_id, config) {
+    console.log("Creating", widget_id, config);
+    this.widget_id = widget_id;
+    this.title = config.title;
+    this.max_items = config.nitems;
+    this.messages = [];
+    this.element = new Element('div');
+    $('widgets').appendChild(this.element);
+  },
+  receive: function(message) {
+    if(this.messages.length >= this.max_items) {
+      this.messages.shift();
     }
+    this.messages.push(message);
+    this.update();
+  },
+
+  update: function() {
+    // remove existing messages
+    this.element.childElements().invoke('remove');
+    this.messages.each(function(message) {
+      console.log("Rendering", message.user, message.text);
+      var cont = new Element('p');
+      cont.appendChild(new Element('a', { href: 'http://www.twitter.com/' + message.user, class: 'author' }).update(message.user));
+      cont.appendChild(document.createTextNode(message.text));
+      this.element.appendChild(cont);
+    }.bind(this));
+  }
 });
