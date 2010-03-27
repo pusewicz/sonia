8e379897f9b2a578ff52d6a5ac168be077bab262
Piotr Usewicz <piotr@layer22.com>
2010-03-27 18:40:35 +0000

    Simple tweaks

--- public/javascripts/sonia.js
+++ public/javascripts/sonia.js
@@ -1,4 +1,3 @@
-console.log("Sonia");
 var Sonia = Class.create({
   widgets: {},
   initialize: function(host) {
@@ -50,7 +49,6 @@ var Dispatcher = Class.create({
         var widget_id = payload.widget_id;
         var config    = payload.config;
         if(widget && widget_id && config) {
-          console.log("new " + widget + "(widget_id, config)");
           var widget_object = eval("new " + widget + "(widget_id, config)");
           this.sonia.addWidget(widget_id, widget_object);
         } else {
@@ -61,32 +59,56 @@ var Dispatcher = Class.create({
   }
 });
 
-var Twitter = Class.create({
+var Widget = Class.create({
+  parent: $('widgets'),
+
   initialize: function(widget_id, config) {
-    console.log("Creating", widget_id, config);
     this.widget_id = widget_id;
-    this.title = config.title;
-    this.max_items = config.nitems;
+    this.title     = config.title;
+    this.config    = config;
+    this.buildContainer();
+    this.update();
+  },
+
+  receive: function(message) {
+    console.log(this.title, "received message: ", message);
+  },
+
+  update: function() {
+    console.log(this.title, "should redraw");
+  },
+
+  buildContainer: function() {
+    this.container = new Element('div', {id: this.widget_id, class: "widget"});
+    $('widgets').appendChild(this.container);
+  }
+});
+
+var Twitter = Class.create(Widget, {
+  initialize: function($super, widget_id, config) {
     this.messages = [];
-    this.element = new Element('div');
-    $('widgets').appendChild(this.element);
+    return($super(widget_id, config));
   },
   receive: function(message) {
-    if(this.messages.length >= this.max_items) {
+    if(this.messages.length >= this.config.nitems) {
       this.messages.shift();
     }
     this.messages.push(message);
     this.update();
   },
   update: function() {
-    this.element.childElements().invoke('remove');
+    this.container.childElements().invoke('remove');
+    this.container.appendChild(this.buildHeader());
     this.messages.each(function(message) {
-      console.log("Rendering", message.user, message.text);
       var cont = new Element('p');
       cont.appendChild(new Element('img', { src: message.profile_image_url, width: 48, height: 48 }));
       cont.appendChild(new Element('a', { href: 'http://www.twitter.com/' + message.user, class: 'author' }).update(message.user));
       cont.appendChild(document.createTextNode(message.text));
-      this.element.appendChild(cont);
+      this.container.appendChild(cont);
     }.bind(this));
+  },
+
+  buildHeader: function() {
+    return(new Element("h2").update(this.title));
   }
 });
