ca54509c7c39c25fdf16dcf3e241f55264a7642e
Piotr Usewicz <piotr@layer22.com>
2010-04-23 13:58:04 +0100

    Add async http requests

--- lib/sonia/widgets/campfire.rb
+++ lib/sonia/widgets/campfire.rb
@@ -9,19 +9,18 @@ module Sonia
   module Widgets
     class Campfire < Sonia::Widget
       GRAVATAR_URL = "http://www.gravatar.com/avatar/"
-      
+
       def initialize(config)
         super(config)
         @room_uri = URI.encode("#{config[:url]}/room/#{config[:room_id]}.json")
-        user_info()
-        connect_to_stream()
+        user_info
+        connect_to_stream
 
-        EventMachine::add_periodic_timer(150) { user_info() }
+        EventMachine::add_periodic_timer(150) { user_info }
       end
 
       private
       def connect_to_stream
-        puts "Connecting to campfire: #{config.inspect}"
         @stream = ::Twitter::JSONStream.connect(
           :path => "/room/#{config[:room_id]}/live.json",
           :host => 'streaming.campfirenow.com',
@@ -30,14 +29,13 @@ module Sonia
 
         @stream.each_item do |message|
           json_message = Yajl::Parser.parse(message)
-          puts json_message
           formatted = format_message(json_message)
           push formatted unless formatted.nil?
         end
       end
 
       def format_message(message)
-        if message['type']=='TextMessage'
+        if message['type'] == 'TextMessage'
           user = @users[message['user_id']]
           img = user.nil? ? '' : user_gravatar(user[:email])
           name = user.nil? ? 'Unknown' : user[:name]
@@ -50,23 +48,26 @@ module Sonia
         return nil
       end
 
-      def user_info()
-        puts "Getting user info"
+      def user_info
         @users ||= {}
-        room_info = HTTParty.get(@room_uri, :format => :json, :basic_auth => {:username => config[:token], :password => 'x'})
-        puts room_info.inspect
-        room_info['room']['users'].each do |user|
-          @users[user['id']] = {
-            :name => user['name'],
-            :email => user['email_address']
-          }
-        end
-        puts @users.inspect
+        http = EventMachine::HttpRequest.new(@room_uri).get(headers)
+        http.callback {
+          Yajl::Parser.parse(http.response)['room']['users'].each do |user|
+            @users[user['id']] = {
+              :name  => user['name'],
+              :email => user['email_address']
+            }
+          end
+        }
       end
 
       def user_gravatar(email)
         email_digest = Digest::MD5.hexdigest(email)
         "#{GRAVATAR_URL}#{email_digest}?d=identicon"
+      end
+
+      def headers
+        { :head => { 'Authorization' => [config[:token], "x"] } }
       end
     end
   end
