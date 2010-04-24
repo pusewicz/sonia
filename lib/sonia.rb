d47242994ffa2d6f9d85cad21692f171b79c2391
Piotr Usewicz <piotr@layer22.com>
2010-04-24 14:14:19 +0100

    Add logger

--- lib/sonia.rb
+++ lib/sonia.rb
@@ -1,7 +1,13 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 
+require 'logger'
+
 module Sonia
   autoload :Server, 'sonia/server'
   autoload :Widget, 'sonia/widget'
   autoload :Widgets, 'sonia/widgets'
+
+  def self.log
+    @logger ||= Logger.new(STDOUT)
+  end
 end
