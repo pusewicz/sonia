967245cafa40f5453e3097ba9d72d67296eccf4b
Piotr Usewicz <piotr@layer22.com>
2010-05-01 16:54:04 +0100

    Add console task

--- lib/sonia/cli.rb
+++ lib/sonia/cli.rb
@@ -1,4 +1,3 @@
-require "sonia"
 require 'thor'
 require "launchy"
 
@@ -9,9 +8,19 @@ module Sonia
     desc "start", "Start Sonia server"
     method_option :config, :type => :string, :aliases => "-c", :required => true
     def start
+      require "sonia"
       Sonia::Server.run!(Config.new(options)) do
         Launchy::Browser.run(Sonia::Server.webserver_url)
       end
+    end
+
+    desc "console", "Start Sonia console"
+    def console
+      ARGV.pop # Remove console as parameter
+      require 'irb'
+      require 'irb/completion'
+      require 'sonia'
+      IRB.start(__FILE__)
     end
   end
 end
