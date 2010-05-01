472ef4c29668967495ff82109cdb166db14b99e2
Piotr Usewicz <piotr@layer22.com>
2010-05-01 15:32:57 +0100

    Add Sonia::Helpers for Sinatra
    Currently comes with widget_javascripts which
    returns an array of all widget js relative paths

--- lib/sonia.rb
+++ lib/sonia.rb
@@ -3,13 +3,18 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 require 'logger'
 
 module Sonia
-  autoload :Server, 'sonia/server'
-  autoload :Widget, 'sonia/widget'
-  autoload :Widgets, 'sonia/widgets'
+  autoload :Server,    'sonia/server'
+  autoload :Widget,    'sonia/widget'
+  autoload :Widgets,   'sonia/widgets'
   autoload :WebServer, 'sonia/web_server'
-  autoload :Config, 'sonia/config'
+  autoload :Config,    'sonia/config'
+  autoload :Helpers,   'sonia/helpers'
 
   def self.log
     @logger ||= Logger.new(STDOUT)
+  end
+
+  def self.root
+    @@root ||= File.expand_path(File.join(File.dirname(__FILE__), '..'))
   end
 end
