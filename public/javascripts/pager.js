618509d672f3ec0d7b3b5d712f0ea4942b198f23
Piotr Usewicz <piotr@layer22.com>
2010-05-04 19:35:45 +0100

    Add ability to add widgets to a page

--- public/javascripts/pager.js
+++ public/javascripts/pager.js
@@ -1,9 +1,20 @@
 var Pager = Class.create({
   initialize: function(sonia) {
     this.sonia = sonia;
-    this.pages = [["sha1"], ["sha2"], ["sha3"]];
+    this.pages = [];
     this.currentPage = 0;
     this.build();
+  },
+
+  addWidgetToCurrentPage: function(widgetSha) {
+    if(!this.pages[this.currentPage]) {
+      this.pages[this.currentPage] = [];
+    }
+
+    this.getCurrentPage().push(widgetSha);
+    this.update();
+    // TODO: Remove this in final version
+    if(this.currentPage == 0) this.currentPage++;
   },
 
   build: function() {
@@ -12,14 +23,51 @@ var Pager = Class.create({
 
   update: function() {
     this.buildPager();
+    this.showOnlyCurrentPageWidgets();
   },
 
   buildPager: function() {
-    if(this.pager) this.pager.remove();
-    this.pager = new Element("ul", { id: "pager" });
+    if(this.pagerContainer) { $(this.pagerContainer).remove(); }
+    this.pagerContainer = new Element("ul", { id: "pager" });
     this.pages.each(function(page) {
-      this.pager.insert(new Element("li", { 'class': (this.pages[this.currentPage] == page) ? 'current' : '' }).update("&bull;"));
+      this.pagerContainer.insert(new Element("li", { 'class': (this.getCurrentPage() == page) ? 'current' : '' }).update("&bull;"));
     }.bind(this));
-    $("widgets").insert(this.pager);
+    $("widgets").insert(this.pagerContainer);
+  },
+
+  showOnlyCurrentPageWidgets: function() {
+    this.currentPageWidgets().each(function(el) {
+      console.log("Current wudget", el);
+      $(el).show();
+    }.bind(this));
+
+    this.notCurrentPageWidgets().each(function(el) {
+      console.log("NotCurrent wudget", el);
+      $(el).hide();
+    }.bind(this));
+  },
+
+  currentPageWidgets: function() {
+    return(this.getCurrentPage().collect(function(el) {
+      return $(el);
+    }.bind(this)));
+  },
+
+  notCurrentPageWidgets: function() {
+    console.log("Pages", this.pages);
+    console.log("CurrPage", this.currentPage);
+    var nonCurrentPages = this.pages.findAll(function(el) {
+      el != this.getCurrentPage();
+    }.bind(this));
+
+    console.log("Noncurrentpages:", nonCurrentPages);
+
+    return(nonCurrentPages.flatten().collect(function(el) {
+      return $(el);
+    }));
+  },
+
+  getCurrentPage: function() {
+    return(this.pages[this.currentPage]);
   }
 });
