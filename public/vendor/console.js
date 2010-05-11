d20be41da674cbe87ee994fc00dc4465c550dc7a
Piotr Usewicz <piotr@layer22.com>
2010-05-11 19:29:51 +0100

    Add console mock

---
+++ public/vendor/console.js
@@ -0,0 +1,9 @@
+if (!window.console) {
+    window.console = {
+        log: function () {},
+        group: function () {},
+        error: function () {},
+        warn: function () {},
+        groupEnd: function () {}
+    };
+}
